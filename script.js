ccb336f660a0872b18a213542258d51a5b560c25
Adelin Prelipcean <aamoviescontact@gmail.com>
2022-10-29 21:42:14 +0100

    V2
    Play 5 games and show the result

--- script.js
+++ script.js
@@ -2,7 +2,52 @@ var items = Array("rock", "paper", "scissors");
 
 function getComputerChoice(){
     var item = items[Math.floor(Math.random()*items.length)];
-    console.log(item);
+    return item;
 }
 
-getComputerChoice();
+const playerSelection = "rock";
+let computerScore = 0;
+let playerScore = 0;
+
+function playGame(){
+    let computerSelection = getComputerChoice();
+    if (computerSelection === playerSelection.toLowerCase()){
+        console.log("TIE!");
+    }
+    else if (computerSelection === "rock" && playerSelection.toLowerCase() === "paper"){
+        console.log("YOU WON! PAPER BEATS ROCK!");
+        playerScore++;
+    }
+    else if (computerSelection === "rock" && playerSelection.toLowerCase() === "scissors"){
+        console.log("YOU LOST! ROCK BEATS SCISSORS");
+        computerScore++;
+    }
+    else if (computerSelection === "paper" && playerSelection.toLowerCase() === "rock"){
+        console.log("YOU LOST! PAPER BEATS ROCK!");
+        computerScore++;
+    }
+    else if (computerSelection === "paper" && playerSelection.toLowerCase() === "scissors"){
+        console.log("YOU WON! SCISSORS BEATS PAPER");
+        playerScore++;
+    }
+    else if (computerSelection === "scissors" && playerSelection.toLowerCase() === "paper"){
+        console.log("YOU LOST! SCISSORS BEATS PAPER");
+        computerScore++;
+    }
+    else {
+        console.log("YOU WON! ROCK BEATS SCISSORS");
+        playerScore++;
+    }
+}
+
+for (let i = 0; i < 5; i++){
+    console.log(playGame());
+}
+
+if (computerScore > playerScore){
+    alert("You lost!");
+}
+else {
+    alert("You won!!!!!");
+}
+
